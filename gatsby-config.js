ed04e9973783d5f06cc30a7c227e83691127e13e
Jacqueline Nguyen <beuyenj@gmail.com>
2020-07-30 01:48:46 -0700

    Installed source-filesystem plugin package.

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,13 @@ module.exports = {
   },
   /* Your site config here */
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    }
   ]
 }
