53708153efb963e285fc38185cadde9802f22102
Jacqueline Nguyen <beuyenj@gmail.com>
2020-07-31 01:10:10 -0700

    Added 404 page and Head

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 const ContactPage = () => {
     return (
         <Layout>
+            <Head title="Contact"/>
             <h1>Contact</h1>
             <p>
                 The best way to contact me is through my Twitter profile <a href="https://twitter.com/kanyewest" target="_blank">@kanyewest</a></p>       
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 const IndexPage = () => {
   return (
     <Layout>
+      <Head title="Home"/>
       <h1>Hello!</h1>
       <h2>I'm Jacqueline, a recent UCLA grad living in the Bay Area.</h2>
       <p>Need a developer? <Link to="/contact">Contact me.</Link> </p>
